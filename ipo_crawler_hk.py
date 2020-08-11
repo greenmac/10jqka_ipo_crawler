9334512b7cedf219514cca73e03f97ad4fa227f4
greenmac <alwaysmac@msn.com>
2020-08-11 17:37:25 +0800

    fix headers

--- ipo_crawler_hk.py
+++ ipo_crawler_hk.py
@@ -22,15 +22,15 @@ def HKNewStockCalendar(page_nums=5):
         'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6',
         'Cache-Control': 'max-age=0',
         'Connection': 'keep-alive',
-        'Cookie': 'Hm_lvt_78c58f01938e4d85eaf619eae71b4ed1=1595991283; __utma=156575163.1303265994.1596075105.1596075105.1596075105.1; __utmc=156575163; __utmz=156575163.1596075105.1.1.utmcsr=10jqka.com.cn|utmccn=(referral)|utmcmd=referral|utmcct=/; Hm_lvt_60bad21af9c824a4a0530d5dbf4357ca=1595991283,1596093102,1596093500; Hm_lvt_f79b64788a4e377c608617fba4c736e2=1595991283,1596093102,1596093500; historystock=HK9913; Hm_lpvt_60bad21af9c824a4a0530d5dbf4357ca=1596594795; Hm_lpvt_78c58f01938e4d85eaf619eae71b4ed1=1596594795; Hm_lpvt_f79b64788a4e377c608617fba4c736e2=1596594795; v=Av40TFDtLYCif3mnwhuCxXmdSR9DP8K5VAN2nagHasE8S5AJEM8SySSTxqF7',    
+        'Cookie': 'Hm_lvt_78c58f01938e4d85eaf619eae71b4ed1=1595991283; __utma=156575163.1303265994.1596075105.1596075105.1596075105.1; __utmc=156575163; __utmz=156575163.1596075105.1.1.utmcsr=10jqka.com.cn|utmccn=(referral)|utmcmd=referral|utmcct=/; Hm_lvt_60bad21af9c824a4a0530d5dbf4357ca=1595991283,1596093102,1596093500; Hm_lvt_f79b64788a4e377c608617fba4c736e2=1595991283,1596093102,1596093500; historystock=HK9913; Hm_lpvt_78c58f01938e4d85eaf619eae71b4ed1=1597116011; Hm_lpvt_f79b64788a4e377c608617fba4c736e2=1597116011; Hm_lpvt_60bad21af9c824a4a0530d5dbf4357ca=1597116011; v=AuogCJyJQYy2Q817qn_eWe0xPVuPW261YN_iWXSjlj3Ip4RNXOu-xTBvMmVH',
         'Host': 'data.10jqka.com.cn',
         'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3554.0 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
     }
 
     # 港股新股
     new_stocks = []
-    for page_num in range(page_nums+1):
+    for page_num in range(1, page_nums+1):
         url = f'http://data.10jqka.com.cn/ipo/xgsgyzq/board/hkstock/field/end_buy_time/page/{page_num}/order/desc/ajax/1/'
         res = requests.get(url, headers=headers, proxies=proxies)
         res.encoding = 'GBK'
